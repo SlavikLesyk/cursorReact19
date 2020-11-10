227ec4db1ae22aabc718330565c045403acceb55
Yaroslav Lesyk <slaviklesyk706@gmail.com>
2020-11-10 13:32:36 +0200

    change to HashRouter nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import SignIn from './SignIn/SignIn';
 import './App.css'
 import SignOut from './SignOut/SignOut';
@@ -10,13 +10,13 @@ const App = () => {
     <Router>
       <div>
       <Switch>
-        <Route path={process.env.PUBLIC_URL + '/SignIn'} component={SignIn} />
-        <Route path={process.env.PUBLIC_URL + '/SignOut'} component={SignOut} />
-        <Route exact path={process.env.PUBLIC_URL + '/'}  render={() => <Redirect to="/SignIn" />} />
+        <Route path='/SignIn' component={SignIn} />
+        <Route path='/SignOut' component={SignOut} />
+        <Route exact path='/'  render={() => <Redirect to="/SignIn" />} />
       </Switch>
       </div>
     </Router>
-  );    
+  );
 }
 
 export default App;
